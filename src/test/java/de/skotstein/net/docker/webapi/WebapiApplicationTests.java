7df46f790acb8d5b83fb60ae3942723d480c3779
Sebastian Kotstein <sebastian.kotstein@gmx.de>
2023-11-13 21:19:44 +0100

    Initial commit

---
+++ src/test/java/de/skotstein/net/docker/webapi/WebapiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.skotstein.net.docker.webapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
